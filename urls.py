edbca24b48ec740db3d9aa69f3572e66a8b4c55e
NeoUKR <neokkv@gmail.com>
2022-05-22 19:44:38 +0300

    Fix routes

--- urls.py
+++ urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from .views import index
 
 urlpatterns = [
-    path('tb', index)
-    ,path('tb/dashboard', index)
-    ,path('tb/dashboard/monitor', index)
-    ,path('tb/dashboard/tasklist', index)
-    ,path('tb/dashboard/taskelement', index)
+    path('', index),
+    path('monitor', index),
+    path('tasklist', index),
+    path('taskelement', index),
 ]
